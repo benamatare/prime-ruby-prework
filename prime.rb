16ca6a02cf29d03bd0535cfbc0415dd5f3180c1b
Ben Matare <ben.a.matare@gmail.com>
2018-01-19 07:07:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 
-def prime? (integer)
-
+def prime? integer
+  if integer <= 1 
+    return false
+  end
+  
   (2..integer-1).each do |num|
-    if integer % num == 0
-      return false
+    if integer % num == 0 
+      return false 
     end
   end
   return true
